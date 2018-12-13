ca64b27dc3fff837db469a2f7feba00243229969
Freddy <freddy.jd2@gmail.com>
2018-12-13 00:05:46 -0500

    Added else and if statements when the user choose R/P/S

--- game.js
+++ game.js
@@ -13,19 +13,19 @@
 // 6) IF/Else statement to see if they have the correct statement
 // 7) Add (1) to a leaderboard
 // Quick note == 
-    // R = 1
-    // P = 2
-    // S = 3 
-// Combos 13, 21, 32 (Users win) --- 12, 23, 31 (Computer win) 11, 22, 33 (tie)
-
-var computerSelection = Math.floor(Math.random() * 3);
-console.log(computerSelection)
+    // R = 0
+    // P = 1
+    // S = 2 
+// Combos 02, 10, 21 (Users win) --- 01, 12, 20 (Computer win) 00, 11, 22 (tie)
 
 var userWin = 0;
 var computerWin = 0;
 var gameTie = 0;
 
 document.onkeyup = function(event) {
+    var computerSelection = Math.floor(Math.random() * 3);
+    console.log("Computer choosed " + computerSelection)
+
     var userSelection = event.key
     if (userSelection === "r") { 
         var userSelection = "0"; 
@@ -33,13 +33,31 @@ document.onkeyup = function(event) {
     } else if (userSelection === "p") {
         var userSelection = "1";
 
-    } else  { 
+    } else { 
         var userSelection = "2"; 
 };
-
 var userSelection = userSelection; 
-if (userSelection + computerSelection === "12") { 
-    console.log("You won!")
-}
-} 
 
+if (userSelection + computerSelection === "02") { 
+    console.log("You won ==> Rock v/s Scissors ==> User wins ")
+
+} else if (userSelection + computerSelection == "10") { 
+    console.log("You won ==> Papper v/s Rock ==> User wins ") 
+
+} else if (userSelection + computerSelection == "21") { 
+    console.log("You won ==> Scissors v/s Paper ==> User wins ") 
+
+} else if (userSelection + computerSelection == "01") { 
+    console.log("You Lose ==> Rock v/s Paper ==> Computer wins ") 
+
+} else if (userSelection + computerSelection == "12") { 
+    console.log("You Lose ==> Paper v/s Scissors ==> Computer wins ") 
+
+} else if (userSelection + computerSelection == "20") { 
+    console.log("You Lose ==> Scissors v/s Rock ==> Computer wins ") 
+
+} else if (userSelection + computerSelection == 11 || 22 || 33) {
+    console.log("Tie! ")
+}
+
+}
